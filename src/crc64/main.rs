7dfde1c10b0205e165dc6cb729b47dc313e43ffa
Jan-Erik Rediger <badboy@archlinux.us>
2014-10-15 18:19:41 +0200

    CRC64 working.

---
+++ src/crc64/main.rs
@@ -0,0 +1,38 @@
+#![feature(slicing_syntax)]
+
+extern crate crc64;
+use crc64::crc64;
+use std::os;
+use std::io::{BufferedReader, File};
+
+fn main() {
+    let args = os::args();
+    if args.len() == 1 {
+        println!("Usage: {} [list of files]", args[0]);
+    } else {
+        for f in args[1..].iter() {
+            let mut crc : u64 = 0;
+            let file = File::open(&Path::new(f.to_string()));
+            let mut reader = BufferedReader::new(file);
+
+            let mut error = false;
+            loop {
+                let mut buf = [0, ..100];
+                match reader.read(buf) {
+                    Err(e) => {
+                        if e.kind != std::io::EndOfFile {
+                            error = true;
+                            print!("error reading '{}': {}", f, e);
+                        }
+                        break;
+                    },
+                    Ok(nread) => crc = crc64::crc64(crc, buf[..nread])
+                }
+            }
+
+            if error == false {
+                println!("{:x}  {}", crc, f);
+            }
+        }
+    }
+}
