5f06bcc3fd868a956c449ace2b600d740255a679
Jan-Erik Rediger <janerik@fnordig.de>
2015-01-23 19:14:48 +0100

    Build main cli tool again

--- src/main.rs
+++ src/main.rs
@@ -9,30 +9,31 @@ fn main() {
     let args = os::args();
     if args.len() == 1 {
         println!("Usage: {} [list of files]", args[0]);
-    } else {
-        for f in args[1..].iter() {
-            let mut crc : u64 = 0;
-            let file = File::open(&Path::new(f.to_string()));
-            let mut reader = BufferedReader::new(file);
+        return
+    }
 
-            let mut error = false;
-            loop {
-                let mut buf = [0; 100];
-                match reader.read(buf.as_mut_slice()) {
-                    Err(e) => {
-                        if e.kind != std::io::EndOfFile {
-                            error = true;
-                            print!("error reading '{}': {}", f, e);
-                        }
-                        break;
-                    },
-                    Ok(nread) => crc = crc64::crc64(crc, buf[..nread])
-                }
-            }
+    for f in args[1..].iter() {
+        let mut crc : u64 = 0;
+        let file = File::open(&Path::new(f.to_string()));
+        let mut reader = BufferedReader::new(file);
 
-            if error == false {
-                println!("{:x}  {}", crc, f);
+        let mut error = false;
+        loop {
+            let mut buf = [0; 100];
+            match reader.read(buf.as_mut_slice()) {
+                Err(e) => {
+                    if e.kind != std::io::EndOfFile {
+                        error = true;
+                        print!("error reading '{}': {}", f, e);
+                    }
+                    break;
+                },
+                Ok(nread) => crc = crc64::crc64(crc, buf[..nread].as_slice())
             }
+        }
+
+        if error == false {
+            println!("{:x}  {}", crc, f);
         }
     }
 }
