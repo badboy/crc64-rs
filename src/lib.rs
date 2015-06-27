c52f742f0723ecd6da2ba2b3d3b675770a644a6c
Sebastian Waisbrot <seppo0010@gmail.com>
2015-06-27 18:55:58 -0500

    Implement Crc64 as a struct with Write

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,8 @@
 /// crc64::crc64(0, "123456789".as_bytes());
 /// ```
 
+use std::io;
+use std::io::Write;
 use std::mem;
 use crc_table::CRC64_TAB;
 mod crc_table;
@@ -144,7 +146,46 @@ pub fn crc64(crc: u64, data: &[u8]) -> u64 {
     crc
 }
 
+pub struct Crc64 {
+    crc64: u64,
+}
+
+impl Crc64 {
+    pub fn new() -> Crc64 {
+        Crc64 { crc64: 0 }
+    }
+    pub fn get(&self) -> u64 {
+        self.crc64.clone()
+    }
+}
+
+impl Write for Crc64 {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.crc64 = crc64(self.crc64, buf);
+        Ok(buf.len())
+    }
+    fn flush(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+}
+
 #[test]
 fn test_crc64_works() {
     assert_eq!(0xe9c6d914c4b8d9ca, crc64(0, "123456789".as_bytes()))
+}
+
+#[test]
+fn test_crc64_write() {
+    let step1 = "12345".as_bytes();
+    let step2 = "6789".as_bytes();
+    let value1 = 17326901458626182669;
+    let value2 = 16845390139448941002;
+    assert_eq!(value1, crc64(0, &step1));
+    assert_eq!(value2, crc64(value1.clone(), &step2));
+
+    let mut crc = Crc64::new();
+    assert_eq!(crc.write(&step1).unwrap(), step1.len());
+    assert_eq!(value1, crc.get());
+    assert_eq!(crc.write(&step2).unwrap(), step2.len());
+    assert_eq!(value2, crc.get());
 }
